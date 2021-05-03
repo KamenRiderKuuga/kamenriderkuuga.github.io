c0ee3406a7c2e153dba7cadef333e2f2915e25ac
gadhao <39508075+GadHao@users.noreply.github.com>
2021-05-03 23:34:09 +0800

    feat 更换博客主题为Eks制作的主题

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,50 @@
-var leftTab = document.getElementById('left-tab'),
-    rightTab = document.getElementById('right-tab');
+$(function () {
+    var pageHeight = $(window).height(); //窗口高
+    var scroll = 0; // 网页卷去的头部，先=0
 
-// 垃圾方法，该方法必须等页面加载玩才有效
-// window.onload = function(){
-//   btn[0].onmouseover=function() {
-//     leftTab.style.display="block";
-//     leftTab.setAttribute("class","rotateInUpLeft self-animated");
-//   }
-//   btn[0].onmouseout=function() {
-//     leftTab.setAttribute("class","rotateOutDownLeft self-animated");
-//     // leftTab.style.display="none";
-//   }
-//   btn[1].onmouseover=function() {
-//     rightTab.style.display="block";
-//     rightTab.setAttribute("class","rotateInUpRight self-animated");
-//   }
-//   btn[1].onmouseout=function() {
-//     rightTab.setAttribute("class","rotateOutDownRight self-animated");
-//     // leftTab.style.display="none";
-//   }
-//
-// }
+    addMove();
+    $(window).scroll(function () {
+        //进入视窗时播放 动画
+        scroll = $(this).scrollTop();
+        addMove();
+    });
 
-function showLeft() {
-  leftTab.style.display="block";
-  leftTab.setAttribute("class","rotateInUpLeft self-animated");
-}
+    function addMove() {
+        // 添加move类，这个是用来控制动画的
+        $('.skill span').each(function (i, ele) {
+            var top = $(this).offset().top;
+            if (top <= pageHeight + scroll) {
+                $(this).addClass('move');
+            }
+        });
+    }
 
-function goneLeft() {
-  leftTab.setAttribute("class","rotateOutDownLeft self-animated");
-}
+    $(".nav-btn").click(function () {
+        $(this).toggleClass("rotate");
+        $("header nav").slideToggle();
+    });
+    $("header nav").click(function () {
+        $(this).hide();
+    })
+    // $('.btn-slider').click(function () {
+    //     $('.banner').slideToggle();
+    //     $(this).css("buttom", "-30")
+    // });
 
-function showRight() {
-  rightTab.style.display="block";
-  rightTab.setAttribute("class","rotateInUpRight self-animated");
-}
+    // setInterval(() => {
+    //     $(".banner img").each(function () {
+    //         $(this).fadeToggle(1000);
+    //     });
+    // }, 5000);
 
-function goneRight() {
-  rightTab.setAttribute("class","rotateOutDownRight self-animated");
-}
+
+    // console.log(111);
+    // window.addEventListener('scroll', function () {
+    //     let t = $('body, html').scrollTop();   // 目前监听的是整个body的滚动条距离
+    //     if (t > 0) {
+    //         $('header').addClass('header-active')
+    //     } else {
+    //         $('header').removeClass('header-active')
+    //     }
+    // })
+});
